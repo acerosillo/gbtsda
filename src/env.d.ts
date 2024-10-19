aef49abdc88eed00f555df306c7bf67a0c359462
Ace Rosillo <acerosillo@Aces-MacBook-Pro.local>
2024-10-20 00:28:17 +0100

    changes to env

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,7 +4,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly CONTENTFUL_SPACE_ID: string;
-  readonly CONTENTFUL_DELIVERY_TOKEN: string;
-  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+  readonly VITE_CONTENTFUL_SPACE_ID: string;
+  readonly VITE_CONTENTFUL_DELIVERY_TOKEN: string;
+  readonly VITE_CONTENTFUL_PREVIEW_TOKEN: string;
 }
